1eceb8ad804d6d11daab61d547a6bc67a5cf1e62
cayo cesar <cayocesar.ac@gmail.com>
2022-08-15 22:43:10 -0300

    feat(): Add validation for CreateGame

--- src/MyApp.Application/Games/CreateGameCommand/CreateGameCommandValidator.cs
+++ src/MyApp.Application/Games/CreateGameCommand/CreateGameCommandValidator.cs
@@ -5,6 +5,26 @@ namespace MyApp.Application.Games.CreateGameCommand
 {
     public class CreateGameCommandValidator : AbstractValidator<CreateGameCommand>
     {
+        public CreateGameCommandValidator()
+        {
+            RuleFor(x => x.Description)
+                .NotEmpty()
+                .MaximumLength(500);
+
+            RuleFor(x => x.Name)
+                .NotEmpty()
+                .MaximumLength(100);
+
+            RuleFor(x => x.Publisher)
+                .NotEmpty()
+                .MaximumLength(100);
+
+            RuleFor(x => x.ReleaseDate)
+                .NotEmpty();
+
+            RuleFor(x => x.Genre)
+                .NotEmpty();
+        }
 
     }
 }
